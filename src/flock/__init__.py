b80a8825e91fb390d8358caf01fd7f552c1262b9
Andre Ratzenberger <andre.ratzenberger@whiteduck.de>
2025-02-11 22:45:10 +0100

    remove cli app for merge

--- src/flock/__init__.py
+++ src/flock/__init__.py
@@ -1,20 +1,8 @@
 """Flock package initialization."""
 
 from opentelemetry import trace
-from rich.console import Console
-from rich.prompt import Prompt
 
-from flock.cli.app import AgentManagementApp
 from flock.config import TELEMETRY
-from flock.core.util.cli_helper import display_banner
 
 tracer = TELEMETRY.setup_tracing()
 tracer = trace.get_tracer(__name__)
-console = Console()
-
-
-def main():
-    """Main entry point for the Flock package."""
-    with tracer.start_as_current_span("flock_main") as span:
-        display_banner()
-        AgentManagementApp().run()
