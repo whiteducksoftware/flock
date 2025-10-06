4ee699ff7f65ab237ad2fb55419f5e2fef1647c2
Andre Ratzenberger <andre.ratzenberger@whiteduck.de>
2025-10-07 01:45:47 +0200

    fix: frontend build package

--- src/flock/helper/cli_helper.py
+++ src/flock/helper/cli_helper.py
@@ -4,7 +4,7 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("flock-flow")
 except PackageNotFoundError:
-    __version__ = "0.2.0"
+    __version__ = "0.5.0b"
 
 
 def display_hummingbird():
